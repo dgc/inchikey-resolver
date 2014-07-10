561bb62761466f389afbc839e0462fff4bbd9993
Don Cruickshank <dgc@soton.ac.uk>
2014-07-10 16:12:41 +0100

    Changed product page to include QR code.

--- InChI/routes/product.js
+++ InChI/routes/product.js
@@ -2,9 +2,7 @@
 var qrcode = require(path.resolve(__dirname, '..', 'qrcode'));
 
 exports.show = function (req, res) {
-    
     qrcode.generate(req.params.inchikey, 4, function (svg) {
-        res.contentType('image/svg+xml');
-        res.send(svg);
+        res.render('product.jade', { title: 'Express', qrcode: svg });
     });
 };
