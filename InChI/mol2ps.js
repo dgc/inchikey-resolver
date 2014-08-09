4222ec60e4bdcde09e35e81cd39be0dd9717e77e
Don Cruickshank <dgc@soton.ac.uk>
2014-08-09 02:44:38 -0700

    Added mol2ps support.

---
+++ InChI/mol2ps.js
@@ -0,0 +1,15 @@
+﻿var path = require('path');
+var exec = require('child_process').exec;
+
+var mol2ps_bin_path = path.resolve('mol2ps', 'mol2ps.exe');
+var mol2ps_bin_args = "--output=svg -";
+
+exports.obtain_svg = function (definition, callback) {
+    
+    child = exec(mol2ps_bin_path + " " + mol2ps_bin_args, function (error, stdout, stderr) {
+        callback(error, stdout);
+    });
+    
+    child.stdin.write(definition);
+    child.stdin.end();
+}
---
+++ InChI/test/mol2ps_tests.js
@@ -0,0 +1,35 @@
+﻿var assert = require('assert');
+var path = require('path');
+var fs = require('fs');
+var _ = require('underscore');
+
+function loadFile(filename, callback) {
+    
+    fs.readFile(path.resolve(__dirname, filename), function (err, data) {
+        
+        if (err)
+            throw err;
+        
+        callback(data.toString('utf8'));
+    });
+}
+
+describe('mol2ps external dependency', function () {
+    
+    it('should execute successfully', function (done) {
+        
+        loadFile('fixtures/benzine.mol', function (mol_definition) {
+            
+            var mol2ps = require('../mol2ps');
+            
+            mol2ps.obtain_svg(mol_definition, function (error, svg_text) {
+                
+                assert.equal(error, null);
+                
+                assert.equal(svg_text.substring(0, 5), "<?xml");
+               
+                done();
+            });
+        });
+    });
+})
