d5e07f4c8d0d588060f4f6225da22109c47b82c9
Don Cruickshank <dgc@soton.ac.uk>
2014-11-05 12:59:07 +0000

    Changed stores to have globally addressable names.

--- InChI/store.js
+++ InChI/store.js
@@ -4,54 +4,62 @@ var _ = require('underscore');
 
 exports.obtain = _.memoize(function obtain(store) {
     
-    var storeObject = {
-        data: {},
-        count: 0,
-        nextID: 1
-    };
-    
-    storeObject.findById = function (id) {
-        return storeObject.data[id];
-    }
-    
-    storeObject.add = function (data) {
+    if (global.store_objects == undefined)
+        global.store_objects = {};
+
+    if (global.store_objects[store] == undefined) {
         
-        var id;
+        var storeObject = {
+            data: {},
+            count: 0,
+            nextID: 1
+        };
         
-        if (data.id) {
-            if (storeObject.data[id]) {
-                throw Error("Record identifier already exists: " + id);
+        storeObject.findById = function (id) {
+            return storeObject.data[id];
+        }
+        
+        storeObject.add = function (data) {
+            
+            var id;
+            
+            if (data.id) {
+                if (storeObject.data[id]) {
+                    throw Error("Record identifier already exists: " + id);
+                }
             }
+            
+            if (data.id) {
+                id = data.id;
+            } else {
+                id = storeObject.nextID;
+                storeObject.nextID++;
+            }
+            
+            storedData = _.extend({ id: id }, data);
+            
+            storeObject.data[id] = storedData;
+            storeObject.count = Object.keys(storeObject.data).length;
+            
+            return storedData;
         }
         
-        if (data.id) {
-            id = data.id;
-        } else {
-            id = storeObject.nextID;
-            storeObject.nextID++;
+        storeObject.update = function (id, attributes) {
+            
+            if (storeObject.data[id] == undefined)
+                throw Error("Unknown record: " + id);
+            
+            _.extend(storeObject.data[id], attributes);
+        };
+        
+        storeObject.destroy = function (id) {
+            delete storeObject.data[id];
+            
+            storeObject.count = Object.keys(storeObject.data).length;
         }
-        
-        storedData = _.extend({ id: id }, data);
-        
-        storeObject.data[id] = storedData;
-        storeObject.count = Object.keys(storeObject.data).length;
-        
-        return storedData;
+
+        global.store_objects[store] = storeObject;
     }
     
-    storeObject.update = function (id, attributes) {
-        
-        if (storeObject.data[id] == undefined)
-            throw Error("Unknown record: " + id);
-        
-        _.extend(storeObject.data[id], attributes);
-    };
-    
-    storeObject.destroy = function (id) {
-        delete storeObject.data[id];
-        
-        storeObject.count = Object.keys(storeObject.data).length;
-    }
-    
-    return storeObject;
+    return global.store_objects[store];
 });
